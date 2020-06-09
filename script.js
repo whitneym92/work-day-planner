f6442d74f717b0be2023e6d046c416a395bba360
Whitney Monk <whitneym92@gmail.com>
2020-06-08 21:41:07 -0500

    stored todos from localStorage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    // test times from momemt
+    // test times from momemt.js
     var test = false;
 
     var now = moment().format('MMMM Do YYYY');
@@ -15,4 +15,20 @@ $(document).ready(function() {
 
     var $dateParagraph = $('#currentDay');
     $dateParagraph.text(now);
-}
+
+    //stored todos from localStorage
+    //use JSON parse to make a string an object
+    var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+    if (test) { console.log(storedPlans); }
+
+    //if plans are retrieved from localStorage, update the array
+    if (storedPlans !== null) {
+        planTextArr = storedPlans;
+    } else {
+        planTextArr = new Array(9);
+        planTextArr[4] = "Lets eat some lunch";
+    }
+
+    if (test) { console.log("full array of planned text", planTextArr); }
+});
